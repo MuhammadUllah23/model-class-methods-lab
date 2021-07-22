da0e3c827f758c2a2980bc18f102295c1df97105
Muhammad <muhammadhullah23@gmail.com>
2021-07-22 19:18:13 -0400

    commit tree 2a58eacab4bbab7831347c3e88467885a1919a9e

--- app/models/boat.rb
+++ app/models/boat.rb
@@ -4,26 +4,32 @@ class Boat < ActiveRecord::Base
   has_many    :classifications, through: :boat_classifications
 
   def self.first_five
+    Boat.all.limit(5)
     # all.limit(5)
   end
 
   def self.dinghy
+    Boat.where("length < 20")
     # where("length < 20")
   end
 
   def self.ship
+    Boat.where("length >= 20")
     # where("length >= 20")
   end
 
   def self.last_three_alphabetically
+    Boat.all.order(name: :desc).limit(3)
     # all.order(name: :desc).limit(3)
   end
 
   def self.without_a_captain
+    Boat.where(captain_id: nil)
     # where(captain_id: nil)
   end
 
   def self.sailboats
+    Boat.includes(:classifications).where(classifications: { name: 'Sailboat' })
     # includes(:classifications).where(classifications: { name: 'Sailboat' })
   end
 
@@ -33,13 +39,16 @@ class Boat < ActiveRecord::Base
     # milliseconds whereas it would take whole seconds for Ruby to do the same.
     #
     # joins(:classifications).group("boats.id").having("COUNT(*) = 3").select("boats.*")
+    Boat.joins(:classifications).group("boats.id").having("COUNT(*) = 3").select("boats.*")
   end
 
   def self.non_sailboats
+    Boat.where("id NOT IN (?)", self.sailboats.pluck(:id))
     # where("id NOT IN (?)", self.sailboats.pluck(:id))
   end
 
   def self.longest
+    Boat.order('length DESC').first
     # order('length DESC').first
   end
 end
--- app/models/captain.rb
+++ app/models/captain.rb
@@ -2,22 +2,27 @@ class Captain < ActiveRecord::Base
   has_many :boats
 
   def self.catamaran_operators
+    Captain.includes(boats: :classifications).where(classifications: {name: "Catamaran"})
     # includes(boats: :classifications).where(classifications: {name: "Catamaran"})
   end
 
   def self.sailors
+    Captain.includes(boats: :classifications).where(classifications: {name: "Sailboat"}).distinct
     # includes(boats: :classifications).where(classifications: {name: "Sailboat"}).distinct
   end
 
   def self.motorboat_operators
+    Captain.includes(boats: :classifications).where(classifications: {name: "Motorboat"})
     # includes(boats: :classifications).where(classifications: {name: "Motorboat"})
   end
 
   def self.talented_seafarers
+    Captain.where("id IN (?)", self.sailors.pluck(:id) & self.motorboat_operators.pluck(:id))
     # where("id IN (?)", self.sailors.pluck(:id) & self.motorboat_operators.pluck(:id))
   end
 
   def self.non_sailors
+    Captain.where("id NOT IN (?)", self.sailors.pluck(:id))
     # where.not("id IN (?)", self.sailors.pluck(:id))
   end
 
